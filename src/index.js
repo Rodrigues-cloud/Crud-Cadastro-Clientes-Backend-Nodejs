a9305af1920ff1bd80c56caa53d051dee6ff142a
Rodrigues-cloud <uesleyrs2020@outlook.com>
2021-03-15 01:22:45 -0300

    update arquivo index

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes); 
 
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
     console.log('API ONLINE');
 });
