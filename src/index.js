ce478e744e9725332988fb54660adc6dbb406a70
Rodrigues-cloud <uesleyrs2020@outlook.com>
2021-03-15 10:04:07 -0300

    configuration port 3000

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes); 
 
 
-server.listen(process.env.PORT || 3333);
+//server.listen(process.env.PORT || 3333);
 
-/*server.listen(3333, () => {
+server.listen(3000, () => {
     console.log('API ONLINE');
-});*/
+});
