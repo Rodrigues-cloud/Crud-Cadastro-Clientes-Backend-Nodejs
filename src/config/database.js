a1570c8f28aeb545a333d21345fd58db2125ccec
Rodrigues-cloud <uesleyrs2020@outlook.com>
2021-03-16 17:53:06 -0300

    update string connection mongodb

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/clientes';
+const url = 'mongodb+srv://deploy:uploaddeploy@cluster0.5or3g.mongodb.net/deploy?retryWrites=true&w=majority';
 mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+require('dotenv').config();
+
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes); 
 
 
-//server.listen(process.env.PORT || 3333);
+//server.listen(3333,() =>{});
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
     console.log('API ONLINE');
 });
