1f3c9fb8469b262f848f75464bc14f3c5fe658a6
Rodrigues-cloud <uesleyrs2020@outlook.com>
2021-03-17 12:33:53 -0300

    update file database

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://deploy:uploaddeploy@cluster0.5or3g.mongodb.net/uploadcrud?retryWrites=true&w=majority';
-mongoose.connect(url, { useNewUrlParser: true });
+const MONGO_URL = 'mongodb+srv://deploy:uploaddeploy@cluster0.5or3g.mongodb.net/uploadcrud?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
 
 module.exports = mongoose;
